f969e20700f9901081f8504f5ea35d102f719265
nirav <nirav90up@gmail.com>
2019-04-06 10:09:20 +0530

    code added for searchbar

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from "react";
 import ReactDom from "react-dom";
+import YTSearch from "youtube-api-search";
+
+import SearchBar from "./components/searchbar";
+
+YTSearch({ API_KEY }, function() {});
+const API_KEY = "AIzaSyAuQCVeNfKhtRk9KlChQPT1nO27DPO_5Ss";
 const App = function() {
-  return <div>Hi there! </div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 };
 
 ReactDom.render(<App />, document.querySelector(".root"));
